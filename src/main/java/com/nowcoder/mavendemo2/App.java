5f60b24217575e644a64fa5a86a14613ae98aa2c
wzq <3172540646@qq.com>
2022-03-25 17:01:28 +0800

    test1

---
+++ src/main/java/com/nowcoder/mavendemo2/App.java
@@ -0,0 +1,14 @@
+package com.nowcoder.mavendemo2;
+
+/**
+ * Hello world!
+ *compile
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
